842f61f40004e4575e91b489d62488e0a1c356a3
Liza Morrison <lizammorrison@gmail.com>
2021-06-05 13:12:07 +0200

    add asheville jpg

--- art/src/App.tsx
+++ art/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import asheville from './Liza-Asheville.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={asheville} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Liza Morrison Art
         </p>
         <a
           className="App-link"
