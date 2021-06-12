7bfd59b76c6c2ceab5fb38e163d399a017446ad8
Liza Morrison <lizammorrison@gmail.com>
2021-06-12 14:41:34 +0200

    minor css home changes

--- art/src/Footer/Footer.tsx
+++ art/src/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div className="footer">© Developed by Liza Morrison 2021.</div>;
+  return <div className="footer">© Liza Morrison 2021.</div>;
 };
 
 export default Footer;
