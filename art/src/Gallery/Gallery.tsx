fe2d85657db1521b97a35836d4154dc502e09157
Liza Morrison <lizammorrison@gmail.com>
2021-06-12 15:34:27 +0200

    first photo test

--- art/src/Gallery/Gallery.tsx
+++ art/src/Gallery/Gallery.tsx
@@ -2,8 +2,34 @@ import React, { Component } from 'react';
 
 class Gallery extends Component {
   render() {
-    return <></>;
+    return (
+      <div className="photos">
+        {photos.map((photo, index) => (
+          <img
+            src={photo.src}
+            alt={photo.alt}
+            key={index}
+            height={photo.height}
+            width={photo.width}
+          />
+        ))}
+      </div>
+    );
   }
 }
+
+const photos = [
+  {
+    src: './Images/curry.jpg',
+    width: 400,
+    height: 300,
+    alt: '',
+  },
+  // {
+  //   src: 'http://example.com/example/img2.jpg',
+  //   width: 1,
+  //   height: 1,
+  // },
+];
 
 export default Gallery;
