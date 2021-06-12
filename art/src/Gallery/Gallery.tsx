5629acc3c61fd3245f1f22005ff994cd326cd1cf
Liza Morrison <lizammorrison@gmail.com>
2021-06-12 19:53:39 +0200

    filter images

--- art/src/Gallery/Gallery.tsx
+++ art/src/Gallery/Gallery.tsx
@@ -1,85 +1,53 @@
-import React, { Component } from 'react';
-import curry from './Images/curry.jpg';
-import eagles from './Images/eagles.jpg';
-import odellAndSaquon from './Images/odell-and-saquon.jpg';
-import saquon from './Images/saquon.jpg';
-import fatherDaughter from './Images/father-daughter.jpg';
-import robinWilliams from './Images/robin.jpg';
-import walterWhite from './Images/walter-white.jpg';
-import tevlin from './Images/tevlin.jpg';
-import titanic from './Images/titanic.jpg';
-import titanicFrame from './Images/titanic-frame.jpg';
+import React, { useState } from 'react';
+import { photos } from './GalleryImages';
 
-class Gallery extends Component {
-  render() {
-    return (
-      <div className="gallery-grid">
-        {photos.map((photo, index) => (
-          <img
-            src={photo.src}
-            alt={photo.alt}
-            key={index}
-            height={photo.height}
-            width={photo.width}
-          />
-        ))}
+const Gallery: React.FC = () => {
+  const [images, setImages] = useState(photos);
+
+  return (
+    <div className="gallery-grid">
+      <div className="gallery-type">
+        <span
+          onClick={() =>
+            setImages(photos.filter((img) => img.id.includes('drawing')))
+          }
+        >
+          Drawings
+        </span>
+        <span
+          onClick={() =>
+            setImages(photos.filter((img) => img.id.includes('painting')))
+          }
+        >
+          Paintings
+        </span>
+        <span
+          onClick={() =>
+            setImages(photos.filter((img) => img.id.includes('print')))
+          }
+        >
+          Prints
+        </span>
+        <span
+          onClick={() =>
+            setImages(photos.filter((img) => img.id.includes('misc')))
+          }
+        >
+          Misc
+        </span>
       </div>
-    );
-  }
-}
-
-const photos = [
-  {
-    src: curry,
-    width: 'auto',
-    height: 400,
-    alt: '',
-  },
-  {
-    src: eagles,
-    width: 'auto',
-    height: 300,
-  },
-  {
-    src: odellAndSaquon,
-    width: 'auto',
-    height: 500,
-  },
-  {
-    src: saquon,
-    width: 'auto',
-    height: 300,
-  },
-  {
-    src: fatherDaughter,
-    width: 300,
-    height: 'auto',
-  },
-  {
-    src: robinWilliams,
-    width: 400,
-    height: 'auto',
-  },
-  {
-    src: walterWhite,
-    width: 470,
-    height: 'auto',
-  },
-  {
-    src: tevlin,
-    width: 300,
-    height: 'auto',
-  },
-  {
-    src: titanic,
-    width: 500,
-    height: 'auto',
-  },
-  {
-    src: titanicFrame,
-    width: 500,
-    height: 'auto',
-  },
-];
+      {images.map((photo, index) => (
+        <img
+          src={photo.src}
+          alt={photo.alt}
+          key={index}
+          height={photo.height}
+          width={photo.width}
+          id={photo.id}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Gallery;
---
+++ art/src/Gallery/GalleryImages.ts
@@ -0,0 +1,73 @@
+import curry from './Images/curry.jpg';
+import eagles from './Images/eagles.jpg';
+import odellAndSaquon from './Images/odell-and-saquon.jpg';
+import saquon from './Images/saquon.jpg';
+import fatherDaughter from './Images/father-daughter.jpg';
+import robinWilliams from './Images/robin.jpg';
+import walterWhite from './Images/walter-white.jpg';
+import tevlin from './Images/tevlin.jpg';
+import titanic from './Images/titanic.jpg';
+import titanicFrame from './Images/titanic-frame.jpg';
+
+export const photos = [
+  {
+    src: curry,
+    width: 'auto',
+    height: 400,
+    alt: '',
+    id: 'drawing',
+  },
+  {
+    src: eagles,
+    width: 'auto',
+    height: 300,
+    id: 'drawing',
+  },
+  {
+    src: odellAndSaquon,
+    width: 'auto',
+    height: 500,
+    id: 'drawing',
+  },
+  {
+    src: saquon,
+    width: 'auto',
+    height: 300,
+    id: 'drawing',
+  },
+  {
+    src: fatherDaughter,
+    width: 300,
+    height: 'auto',
+    id: 'drawing',
+  },
+  {
+    src: robinWilliams,
+    width: 400,
+    height: 'auto',
+    id: 'drawing',
+  },
+  {
+    src: walterWhite,
+    width: 470,
+    height: 'auto',
+    id: 'drawing',
+  },
+  {
+    src: tevlin,
+    width: 300,
+    height: 'auto',
+    id: 'drawing',
+  },
+  {
+    src: titanic,
+    width: 500,
+    height: 'auto',
+    id: 'painting',
+  },
+  {
+    src: titanicFrame,
+    width: 500,
+    id: 'painting',
+  },
+];
