7164e2c5d11223042a4b9d4308240976ed297990
Liza Morrison <lizammorrison@gmail.com>
2021-06-14 16:21:38 +0200

    images and scroll to top

--- art/src/Gallery/Gallery.tsx
+++ art/src/Gallery/Gallery.tsx
@@ -3,53 +3,87 @@ import { photos } from './GalleryImages';
 
 const Gallery: React.FC = () => {
   const [images, setImages] = useState(photos);
+  const [isScrollBtnDisplayed, setIsScrollBtnDisplayed] = useState(false);
+
+  const scrollToTop = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    setIsScrollBtnDisplayed(false);
+  };
+  const showScrollToTop = () => {
+    setIsScrollBtnDisplayed(true);
+  };
+  window.onscroll = () => {
+    if (
+      (document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20) &&
+      !isScrollBtnDisplayed
+    )
+      showScrollToTop();
+  };
 
   return (
-    <div className="gallery-grid">
-      <div className="gallery-type">
-        <span
-          onClick={() =>
-            setImages(photos.filter((img) => img.id.includes('drawing')))
-          }
+    <>
+      {isScrollBtnDisplayed && (
+        <div
+          id="scroll-btn"
+          className="scroll-btn"
+          onClick={() => scrollToTop()}
         >
-          Drawings
-        </span>
-        <span
-          onClick={() =>
-            setImages(photos.filter((img) => img.id.includes('painting')))
-          }
-        >
-          Paintings
-        </span>
-        <span
-          onClick={() =>
-            setImages(photos.filter((img) => img.id.includes('print')))
-          }
-        >
-          Prints
-        </span>
-        <span
-          onClick={() =>
-            setImages(photos.filter((img) => img.id.includes('misc')))
-          }
-        >
-          Misc
-        </span>
-      </div>
-      {images.map((photo, index) => (
-        <div className="photo-item">
-          <img
-            src={photo.src}
-            alt={photo.alt}
-            key={index}
-            height={photo.height}
-            width={photo.width}
-            id={photo.id}
-          />
-          <div className="photo-desc">{photo.desc}</div>
+          Top
         </div>
-      ))}
-    </div>
+      )}
+      <div className="gallery-grid">
+        <div className="gallery-type">
+          <span
+            onClick={() =>
+              setImages(photos.filter((img) => img.id.includes('drawing')))
+            }
+          >
+            Drawings
+          </span>
+          <span
+            onClick={() =>
+              setImages(photos.filter((img) => img.id.includes('painting')))
+            }
+          >
+            Paintings
+          </span>
+          <span
+            onClick={() =>
+              setImages(photos.filter((img) => img.id.includes('print')))
+            }
+          >
+            Prints
+          </span>
+          <span
+            onClick={() =>
+              setImages(photos.filter((img) => img.id.includes('misc')))
+            }
+          >
+            Misc
+          </span>
+        </div>
+        {images.map((photo, index) => (
+          <div className="photo-item" key={index}>
+            <img
+              src={photo.src}
+              alt={photo.alt}
+              key={index}
+              id={photo.id}
+              style={{
+                maxWidth: photo.maxWidth,
+                width: '100%',
+                height: 'auto',
+              }}
+            />
+            <div style={{ width: photo.maxWidth }} className="photo-desc">
+              {photo.desc}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
--- art/src/Home/Home.tsx
+++ art/src/Home/Home.tsx
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 import asheville from './Liza-Asheville.jpg';
 
 class Home extends Component {
-  render () {
+  render() {
     return (
       <div className="home-screen">
         <img src={asheville} className="asheville-photo" alt="asheville" />
-        <div className="title">
-          Liza Morrison Art
-        </div>
+        <div className="title">Liza Morrison Art</div>
       </div>
     );
   }
