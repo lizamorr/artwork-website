24785b28fb1114225f00312590aaa62a268cf5a8
Liza Morrison <lizammorrison@gmail.com>
2021-06-19 21:03:11 +0200

    updates

--- src/Burger/Menu/Menu.tsx
+++ src/Burger/Menu/Menu.tsx
@@ -7,7 +7,8 @@ const Menu = () => {
       <div
         className={
           window.location.href.includes('/gallery') ||
-          window.location.href.includes('/about')
+          window.location.href.includes('/about') ||
+          window.location.href.includes('/contact')
             ? 'menu-gallery'
             : 'menu'
         }
