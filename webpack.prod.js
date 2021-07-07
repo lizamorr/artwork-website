10484084d27f3765f61f332e89df5486a3df1f6e
Liza Morrison <lizammorrison@gmail.com>
2021-07-07 19:27:26 +0200

    better performance

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,4 +3,9 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
 });
