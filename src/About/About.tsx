a0e5c036efd799808b75e0a1b96c3885e99e09c1
Liza Morrison <lizammorrison@gmail.com>
2021-07-11 22:37:46 +0200

    lots of good changes

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -6,29 +6,28 @@ const About: React.FC = () => {
   return (
     <>
       <div className="about-container">
-        <img src={parisPhoto} alt="Me in Paris" />
-        <div className="about-text">
-          <div className="about-title">Liza Morrison</div>
-          <div className="about-subtitle">Artist & Software Engineer</div>
-          <div className="about-paragraph">
-            Born and raised in New Jersey, I swapped pre-school for art classes
-            (thanks mom) and continued scribbling on paper. I received my BS in
-            Chemistry from the University of Richmond and claimed Richmond as my
-            second home. After my undergraduate studies, I worked as an
-            associate analytical scientist, web developer, and software
-            engineer. When not acting as Mr. Robot, I'm an artist. The majority
-            of my work after high school has been commissions (thanks to all my
-            awesome customers!). My specialities range across various mediums:
-            graphite, charcoal, pen, oils, acrylic, watercolor, and colored
-            pencils. I currently reside in Belgium with my partner and our three
-            pets (2 dogs and 1 cat). Please contact me with more information
-            about commissions. Thank you for checking out my work.
-          </div>
+        <div className="img-follow">
+          <img src={parisPhoto} alt="Me in Paris" />
           <div className="about-follow">
             Follow:{' '}
             <a href="https://www.instagram.com/lizamorrisonart/">
               @Lizamorrisonart
             </a>
+          </div>
+        </div>
+        <div className="about-text">
+          <div className="about-title">Liza Morrison</div>
+          <div className="about-subtitle">Artist & Software Engineer</div>
+          <div className="about-paragraph">
+            Born and raised in New Jersey, I swapped pre-school for art classes
+            (thanks mom). I received my BS in Chemistry from the University of
+            Richmond and claimed Richmond as my second home. Besides being a
+            scientist, web developer, and software engineer, I'm an Artist. My
+            specialities include: graphite, charcoal, pen, oils, acrylic,
+            watercolor, and colored pencils. I currently reside in Belgium with
+            my partner, Julie, and our three pets (Snickers, Tipper, and
+            Pancake). Please contact me with more information about commissions.
+            Thank you for checking out my work.
           </div>
         </div>
       </div>
--- src/Burger/Menu/Menu.tsx
+++ src/Burger/Menu/Menu.tsx
@@ -8,22 +8,30 @@ const Menu = () => {
         window.location.href.includes('/gallery') ||
         window.location.href.includes('/about') ||
         window.location.href.includes('/contact')
-          ? 'menu-gallery'
+          ? 'menu--white'
           : 'menu'
       }
     >
-      <div>
-        <Link to="/">Home</Link>
-      </div>
-      <div>
-        <Link to="/gallery">Gallery</Link>
-      </div>
-      <div>
-        <Link to="/contact">Contact</Link>
-      </div>
-      <div>
-        <Link to="/about">About</Link>
-      </div>
+      {!window.location.href.endsWith('/') && (
+        <div>
+          <Link to="/">Home</Link>
+        </div>
+      )}
+      {!window.location.href.includes('/gallery') && (
+        <div>
+          <Link to="/gallery">Gallery</Link>
+        </div>
+      )}
+      {!window.location.href.includes('/contact') && (
+        <div>
+          <Link to="/contact">Contact</Link>
+        </div>
+      )}
+      {!window.location.href.includes('/about') && (
+        <div>
+          <Link to="/about">About</Link>
+        </div>
+      )}
     </div>
   );
 };
