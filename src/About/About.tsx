cb2a2737cdbfd801437c914f13035784a421c7c4
Liza Morrison <lizammorrison@gmail.com>
2021-07-12 13:20:05 +0200

    try reloading page

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -29,14 +29,14 @@ const About: React.FC = () => {
             <div className="about-subtitle">Artist & Software Engineer</div>
             <div className="about-paragraph">
               Born and raised in New Jersey, I swapped pre-school for art
-              classes (thanks mom). I received my BS in Chemistry from the
-              University of Richmond and claimed Richmond as my second home.
-              Besides being a scientist, web developer, and software engineer,
-              I'm an Artist. My specialities include: graphite, charcoal, pen,
-              oils, acrylic, watercolor, and colored pencils. I currently reside
-              in Belgium with my partner, Julie, and our three pets (Snickers,
-              Tipper, and Pancake). Please contact me with more information
-              about commissions. Thank you for checking out my work.
+              classes (thanks Mom). I received my Bachelor of Science in
+              Chemistry from the University of Richmond and claimed Richmond as
+              my second home. Aside from being a scientist, web developer, and
+              software engineer, I'm an Artist. My specialities include:
+              graphite, charcoal, pen, oil, acrylic, watercolor, and colored
+              pencils. I currently reside in Belgium with my partner, Julie, and
+              our three pets (Snickers, Tipper, and Pancake). Thank you for
+              checking out my work.
             </div>
           </div>
         </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { Route, BrowserRouter, Switch, Redirect } from 'react-router-dom';
 import About from './About';
 import Burger from './Burger';
 import Contact from './Contact';
@@ -17,6 +17,10 @@ ReactDOM.render(
       <Route path="/gallery" component={Gallery} />
       <Route path="/contact" component={Contact} />
       <Route path="/about" component={About} />
+      <Redirect exact from="/reload" to="/" />
+      <Redirect exact from="/gallery/reload" to="/gallery" />
+      <Redirect exact from="/contact/reload" to="/contact" />
+      <Redirect exact from="/about/reload" to="/about" />
     </Switch>
     <Burger />
   </BrowserRouter>,
