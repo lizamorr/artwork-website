7ab83cfc23b5e0edf2b06a5ea94c3b8855929524
Liza Morrison <lizammorrison@gmail.com>
2022-01-14 17:00:31 +0100

    fix contact form

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import emailjs, { init } from 'emailjs-com';
+import emailjs, { init } from '@emailjs/browser';
 import Footer from '../Footer';
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 import Check from './check.svg';
