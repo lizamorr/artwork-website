832687e49252a82be68f662d529c8152e6ec26e9
Liza Morrison <lizammorrison@gmail.com>
2022-07-21 23:40:59 +0200

    Revert "convert switzerland to webp"
    This reverts commit ec79cf6a2fa0c6c91f6964720a650ceeda55c8bf.

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,6 @@ module.exports = {
     "^.+\\.(js|jsx)$": "babel-jest",
   },
   moduleNameMapper: {
-    ".+\\.(jpg|jpeg|png|webp|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/src/__mocks__/file-mock.js`,
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/src/__mocks__/file-mock.js`,
   },
 };
